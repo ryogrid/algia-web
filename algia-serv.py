87aeee3171743adbce0c6036f233f53f71a15b00
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-02-21 21:33:34 +0900

    implemented post text escape and url string conversion to link.

--- algia-serv.py
+++ algia-serv.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request, Markup
 import subprocess
 from subprocess import PIPE
 import re
+from shlex import quote
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -13,9 +15,21 @@ def index():
         cmdout_lines = cmd_out.split("\n")
         for line in cmdout_lines:
             if re.match(r'.+:\s[0-9a-z]+$', line) != None:
-                lines_inner.append([True,line.split(":")[0]])
+                lines_inner.append([1,line.split(":")[0]])
             else:
-                lines_inner.append([False,line])
+                pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
+                found_url_match = re.search(pattern, line)
+                #line_escaped = str(escape(line))
+                #for url in url_list:
+                #line_url_replaced = line_escaped
+                if not found_url_match:
+                    lines_inner.append([2,line])
+                else:
+                    found_url = found_url_match.group(0)
+                    #line_url_replaced = line_escaped.replace(found_url, "<a href=\"" + escape(found_url) + "\">" + escape(found_url) + "</a>")
+                    lines_inner.append([3,line,escape(found_url)])
+                    app.logger.debug(line)
+                
         
         return render_template('index.html', lines=lines_inner)
             
@@ -27,6 +41,7 @@ def post():
         #post_text = request.form["note"]
         req = request.args
         post_text = req.get("note")
+        post_text = post_text.replace("'","")
         #post_data = request.get_json()
         app.logger.debug(type(post_text))
         app.logger.debug(post_text)
@@ -34,7 +49,10 @@ def post():
         app.logger.debug("exeption!")
         pass
 
-    subprocess.run(f'algia n "{post_text}"', shell=True, stdout=PIPE, stderr=PIPE, text=True, encoding="utf-8")
+    #post_text = quote(escape(post_text))
+    post_text = escape(post_text)
+    #subprocess.run("algia n " + "'" + post_text + "'", shell=True, stdout=PIPE, stderr=PIPE, text=True, encoding="utf-8")
+    subprocess.run(["algia","n", post_text], text=True, encoding="utf-8")
     return index()
 
 app.run(host='0.0.0.0', port=8080, debug=True)
