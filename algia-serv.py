90c90bf8b5d636af5bb4850241fe91946ebeab5a
Ryo Kanbayashi <ryo.contact@gmail.com>
2023-02-24 09:14:29 +0900

    modification for receive post from gas.

--- algia-serv.py
+++ algia-serv.py
@@ -38,11 +38,14 @@ def index():
 def post():
     try:
         app.logger.debug(request.args)
-        #post_text = request.form["note"]
-        req = request.args
-        post_text = req.get("note")
-        post_text = post_text.replace("'","")
+        app.logger.debug(request.form)
         #post_data = request.get_json()
+        #app.logger.debug(post_data)
+        post_text = request.form["note"]
+        #req = request.args
+        #post_text = req.get("note")
+        #post_text = post_text.replace("'","")
+        
         app.logger.debug(type(post_text))
         app.logger.debug(post_text)
     except Exception as e:
@@ -50,12 +53,13 @@ def post():
         pass
 
     #post_text = quote(escape(post_text))
-    post_text = escape(post_text)
+    #post_text = escape(post_text)
     #subprocess.run("algia n " + "'" + post_text + "'", shell=True, stdout=PIPE, stderr=PIPE, text=True, encoding="utf-8")
     subprocess.run(["algia","n", post_text], text=True, encoding="utf-8")
     #return index()
-    return redirect(url_for('index'))
+    #return redirect(url_for('index'))
+    return ""
 
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=10111, debug=True)
 
     
